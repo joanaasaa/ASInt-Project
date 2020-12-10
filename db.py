8d61ca04c4d4f00b5f49f3b423f85c90a0286cf4
Joana Sá <joana.mfmsa@gmail.com>
2020-12-10 18:00:51 +0000

    feat: Videos are displayed in video page and views are iterated

--- db.py
+++ db.py
@@ -14,15 +14,15 @@ DB_FILE = "VQAdb.sqlite"
 new_db = True
 if path.exists(DB_FILE):
     new_db = True
-    log2term('I', 'Database already exists')
+    log2term('D', 'Database already exists')
 else:
-    log2term('I', 'Creating new database file')
+    log2term('D', 'Creating new database file')
 
 Engine = create_engine(f'sqlite:///{DB_FILE}', echo=False)
 Base = declarative_base()
 
 
-######################## USERS ########################
+######################## TABLES ########################
 class User(Base):
     __tablename__ = 'users'
 
@@ -43,89 +43,19 @@ class Admin(Base):
         return f"<Admin(username='{self.username}')>"
 
 
-def NewUser(username=str, email=str, name=str):
-    new_user = User(username=username, email=email, name=name)
-    new_stats = UserStats(username=username)
-    try:
-        session.add(new_user)
-        session.add(new_stats)
-        session.commit()
-        session.close()
-        log2term('D', f'New user with username {new_user.username}')
-        return True
-    except Exception as e:
-        session.rollback()
-        session.commit()
-        session.close()
-        log2term('E', f'{e.__class__.__name__} (when adding user {username})')
-        return False
-
-
-def NewAdmin(username=str):
-    # Check if user exists before promoting it do admin
-    user = GetUser(username)
-    if user == None:
-        log2term('E', f"User {username} can't be admin since it doesn't exist")
-        return False
-
-    # Promote user to admin
-    new_admin = Admin(username=username)
-    try:
-        session.add(new_admin)
-        session.commit()
-        session.close()
-        log2term('D', f'New admin with username {new_admin.username}')
-        return True
-    except Exception as e:
-        session.rollback()
-        session.commit()
-        session.close()
-        log2term(
-            'E',
-            f'{e.__class__.__name__} (when adding admin with username {username})'
-        )
-        return False
-
-
-def GetUser(username=str):
-    user = session.query(User).filter(User.username == username).first()
-    session.close()
-    return user
-
-
-def GetAdmin(username=str):
-    admin = session.query(Admin).filter(Admin.username == username).first()
-    session.close()
-    return admin
-
-
-def ListAllUsers():
-    return session.query(User).all()
-
-
-def ListAllAdmins():
-    return session.query(Admin).all()
-
-
-##################### USER STATS ######################
 class UserStats(Base):
     __tablename__ = 'user_stats'
 
     username = Column(String, ForeignKey('users.username'), primary_key=True)
-    nr_views = Column(Integer, default=0, nullable=False)
-    videos_posted = Column(Integer, default=0, nullable=False)
-    questions_made = Column(Integer, default=0, nullable=False)
-    answers_given = Column(Integer, default=0, nullable=False)
+    views = Column(Integer, default=0, nullable=False)
+    videos = Column(Integer, default=0, nullable=False)
+    questions = Column(Integer, default=0, nullable=False)
+    answers = Column(Integer, default=0, nullable=False)
 
     def __repr__(self):
-        return f"<UserStats(username='{self.username}', nr_views='{self.nr_views}', videos_posted='{self.videos_posted}', questions_made='{self.questions_made}', answers_given='{self.answers_given}')>"
+        return f"<UserStats(username='{self.username}', views='{self.views}', videos='{self.videos}', questions='{self.questions}', answers='{self.answers}')>"
 
 
-def ListAllUserStats():
-    return session.query(UserStats).all()
-
-
-####################### VIDEOS ########################
 class Video(Base):
     __tablename__ = 'videos'
 
@@ -147,44 +77,7 @@ class Video(Base):
             "views": self.views,
         }
 
-def NewVideo(url=str, desc=str, posted_by=str):
-    # Check if user exists before adding video to DB
-    user = GetUser(posted_by)
-    if user == None:
-        log2term('E', f"Video can't be added to database since user {posted_by} doesn't exist")
-        return False
 
-    # Add new video to DB
-    new_video = Video(url=url, desc=desc, posted_by=posted_by)
-    try:
-        session.add(new_video)
-        session.commit()
-        log2term('D', f'New video with ID {new_video.id}')
-        session.close()
-        return True
-    except Exception as e:
-        session.rollback()
-        session.commit()
-        session.close()
-        log2term('E', f'{e.__class__.__name__} (when adding video {desc})')
-        return False
-
-
-def ListAllVideos():
-    return session.query(Video).all()
-
-def GetUserVideos(username=str):
-    user_videos = session.query(Video).filter(Video.posted_by == username).all()
-    session.close()
-    return user_videos
-
-def GetOtherUsersVideos(username=str):
-    other_users_videos = session.query(Video).filter(Video.posted_by != username).all()
-    session.close()
-    return other_users_videos
-
-
-###################### QUESTIONS ######################
 class Question(Base):
     __tablename__ = 'questions'
 
@@ -209,29 +102,6 @@ Video.questions = relationship('Question',
                                back_populates='video')
 
 
-def NewQuestion(question=str, instant=time, username=str, video_id=int):
-    new_question = Question(question=question,
-                            instant=instant,
-                            user_id=username,
-                            video_id=video_id)
-    try:
-        session.add(new_question)
-        session.commit()
-        log2term(
-            'D',
-            f'New question with ID {new_question.id}, regarding video {video_id}'
-        )
-        session.close()
-        return True
-    except:
-        return False
-
-
-def ListAllQuestions():
-    return session.query(Question).all()
-
-
-####################### ANSWERS #######################
 class Answer(Base):
     __tablename__ = 'answers'
 
@@ -251,15 +121,184 @@ Question.answers = relationship('Answer',
                                 order_by=Answer.id,
                                 back_populates='question')
 
+##################### CREATING DB #####################
+Base.metadata.create_all(Engine)
+Session = sessionmaker(bind=Engine, expire_on_commit=False)
+session = scoped_session(Session)
+
+
+###################### FUNCTIONS ######################
+def NewUser(username=str, email=str, name=str):
+    new_user = User(username=username, email=email, name=name)
+    new_stats = UserStats(username=username)
+    try:
+        session.add(new_user)
+        session.add(new_stats)
+        session.commit()
+        session.close()
+        log2term('I', f'New user with username {new_user.username}')
+        return new_user.username
+    except Exception as e:
+        session.rollback()
+        session.commit()
+        session.close()
+        log2term('E', f'{e.__class__.__name__} (when adding user {username})')
+        return None
+
+
+def NewAdmin(username=str):
+    # Check if user exists before promoting it do admin
+    user = GetUser(username)
+    if user == None:
+        log2term('E', f"User {username} can't be admin since it doesn't exist")
+        return None
+
+    # Promote user to admin
+    new_admin = Admin(username=username)
+    try:
+        session.add(new_admin)
+        session.commit()
+        session.close()
+        log2term('I', f'New admin with username {new_admin.username}')
+        return new_admin.username
+    except Exception as e:
+        session.rollback()
+        session.commit()
+        session.close()
+        log2term(
+            'E',
+            f'{e.__class__.__name__} (when adding admin with username {username})'
+        )
+        return None
+
+
+def NewVideo(url=str, desc=str, posted_by=str):
+    # Check if user exists before adding video to DB
+    user = GetUser(posted_by)
+    if user == None:
+        log2term(
+            'E',
+            f"Video can't be added to database since user {posted_by} doesn't exist"
+        )
+        return None
+
+    # Add new video to DB
+    new_video = Video(url=url, desc=desc, posted_by=posted_by)
+    try:
+        session.add(new_video)
+        session.commit()
+        session.close()
+        log2term('I', f'New video with ID {new_video.id}')
+        # Iterate counter for nr of videos posted by the user
+        Add2VideosPosted(posted_by)
+        return new_video.id
+    except Exception as e:
+        session.rollback()
+        session.commit()
+        session.close()
+        log2term('E', f'{e.__class__.__name__} (when adding video {desc})')
+        return None
+
+
+def NewQuestion(question=str, instant=time, username=str, video_id=int):
+    new_question = Question(question=question,
+                            instant=instant,
+                            user_id=username,
+                            video_id=video_id)
+    try:
+        session.add(new_question)
+        session.commit()
+        log2term(
+            'I',
+            f'New question with ID {new_question.id}, regarding video {video_id}'
+        )
+        session.close()
+        return new_question.id
+    except:
+        return None
+
+
+def ListAllUsers():
+    return session.query(User).all()
+
+
+def ListAllAdmins():
+    return session.query(Admin).all()
+
+
+def ListAllUserStats():
+    return session.query(UserStats).all()
+
+
+def ListAllVideos():
+    return session.query(Video).all()
+
+
+def ListAllQuestions():
+    return session.query(Question).all()
+
 
 def ListAllAnswers():
     return session.query(Answer).all()
 
 
-##################### CREATING DB #####################
-Base.metadata.create_all(Engine)
-Session = sessionmaker(bind=Engine, expire_on_commit=False)
-session = scoped_session(Session)
+def GetUser(username=str):
+    user = session.query(User).filter(User.username == username).first()
+    session.close()
+    return user
+
+
+def GetAdmin(username=str):
+    admin = session.query(Admin).filter(Admin.username == username).first()
+    session.close()
+    return admin
+
+
+def GetVideo(id=int):
+    video = session.query(Video).filter(Video.id == id).first()
+    session.close()
+    return video
+
+
+def GetUserVideos(username=str):
+    user_videos = session.query(Video).filter(
+        Video.posted_by == username).all()
+    session.close()
+    return user_videos
+
+
+def GetOtherUsersVideos(username=str):
+    other_users_videos = session.query(Video).filter(
+        Video.posted_by != username).all()
+    session.close()
+    return other_users_videos
+
+
+def Add2VideosPosted(username=str):
+    stats = session.query(UserStats).filter(
+        UserStats.username == username).first()
+    stats.videos += 1
+    session.commit()
+    session.close()
+    log2term('I', f'Iterated posted videos counter for user {username}')
+    return stats.videos
+
+
+def AddView(username=str, video_id=int):
+    # Iterate the user's views
+    user_stats = session.query(UserStats).filter(
+        UserStats.username == username).first()
+    user_stats.views += 1
+
+    # Iterate the video's views
+    video = session.query(Video).filter(Video.id == video_id).first()
+    video.views += 1
+
+    # Save iterations to DB
+    session.commit()
+    session.close()
+    return user_stats.views, video.views
+
 
 ######################## MAIN #########################
 if __name__ == '__main__':
@@ -267,8 +306,10 @@ if __name__ == '__main__':
         NewUser('ist426524', 'joana.sa@tecnico.ulisboa.pt', 'Joana Sá')
         NewAdmin('ist426524')
 
-        NewUser('ist186412', 'filipe.reynaud@tecnico.ulisboa.pt', 'Filipe Reynaud')
-        NewUser('ist187612', 'miguel.de.matos.e.sa@tecnico.ulisboa.pt', 'Miguel Sá')
+        NewUser('ist186412', 'filipe.reynaud@tecnico.ulisboa.pt',
+                'Filipe Reynaud')
+        NewUser('ist187612', 'miguel.de.matos.e.sa@tecnico.ulisboa.pt',
+                'Miguel Sá')
 
         NewVideo(
             'https://www.youtube.com/watch?v=QtMzV73NAgk&ab_channel=PlayStation',
@@ -279,15 +320,16 @@ if __name__ == '__main__':
         NewVideo(
             'https://www.youtube.com/watch?v=Lq594XmpPBg&ab_channel=PlayStation',
             'Horizon Forbidden West - Announcement Trailer | PS5', 'ist426524')
-        NewVideo(
-            'https://www.youtube.com/watch?v=II5UsqP2JAk',
-            'The Last of Us Part II – Release Date Reveal Trailer | PS4', 'ist426524')
-        NewVideo(
-            'https://www.youtube.com/watch?v=iqysmS4lxwQ&ab_channel=IGN',
-            'Ghost of Tsushima - Official Trailer | The Game Awards', 'ist426524')
+        NewVideo('https://www.youtube.com/watch?v=II5UsqP2JAk',
+                 'The Last of Us Part II – Release Date Reveal Trailer | PS4',
+                 'ist426524')
+        NewVideo('https://www.youtube.com/watch?v=iqysmS4lxwQ&ab_channel=IGN',
+                 'Ghost of Tsushima - Official Trailer | The Game Awards',
+                 'ist426524')
         NewVideo(
             'https://www.youtube.com/watch?v=thgb_ZGrM9Q&ab_channel=IGN',
-            'Horizon 2: Forbidden West - Official Reveal Trailer | PS5 Reveal Event', 'ist426524')
+            'Horizon 2: Forbidden West - Official Reveal Trailer | PS5 Reveal Event',
+            'ist426524')
         NewVideo(
             'https://www.youtube.com/watch?v=rTMG51P6F-Q&ab_channel=Zaypixel',
             'Minecraft | How to Build a Farmhouse', 'ist426524')
@@ -302,9 +344,8 @@ if __name__ == '__main__':
             'https://www.youtube.com/watch?v=HCbCMb6nplI&ab_channel=CommonwealthRealm',
             'Top 10 Nintendo Switch Games of All Time!', 'ist186412')
 
-        NewVideo(
-            'https://www.youtube.com/watch?v=cBoPbdOaw7M&ab_channel=IGN',
-            'Cuphead Review', 'ist187612')
+        NewVideo('https://www.youtube.com/watch?v=cBoPbdOaw7M&ab_channel=IGN',
+                 'Cuphead Review', 'ist187612')
 
         print('All videos')
         print(ListAllVideos())
--- proxy.py
+++ proxy.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, url_for, abort, session
+from flask import Flask, redirect, render_template, url_for, abort, session, request
 from flask_dance.consumer import OAuth2ConsumerBlueprint
 from oauthlib.oauth2.rfc6749.errors import TokenExpiredError
 from aux.logs import log2term
@@ -28,36 +28,66 @@ app.register_blueprint(fenix_blueprint)
 
 ######################## FLASK ########################
 @app.route("/API/<string:username>/videos/", methods=['GET'])
-def UserVideos(username):
+def GetVideos(username):
     user_videos = []
     other_users_videos = []
 
     my_videos = db.GetUserVideos(username)
     other_videos = db.GetOtherUsersVideos(username)
 
-    if(my_videos == None):
+    if (my_videos == None):
         log2term('W', f'There were no videos found for user {username}')
-    else:    
+    else:
         for video in my_videos:
             video_dict = video.to_dictionary()
-            user_videos.append(video_dict) # This is a list
+            user_videos.append(video_dict)  # This is a list
 
     # Create a list with all of the videos from other users
-    if(other_videos == None):
-        log2term('W', f'There were no videos found for other users ({username})')
-    else:   
+    if (other_videos == None):
+        log2term('W',
+                 f'There were no videos found for other users ({username})')
+    else:
         for video in other_videos:
             video_dict = video.to_dictionary()
-            other_users_videos.append(video_dict) # This is a list
+            other_users_videos.append(video_dict)  # This is a list
 
-    return {'user_videos': user_videos, "other_users_videos": other_users_videos} # A dict must be returned
-        
+    return {
+        'user_videos': user_videos,
+        "other_users_videos": other_users_videos
+    }  # A dict must be returned
+
+
+@app.route("/API/<string:username>/videos/", methods=['POST'])
+def AddVideo(username):
+    video_data = request.get_json()
+
+    url = video_data["url"]
+    desc = video_data["desc"]
+    video_id = db.NewVideo(url, desc, username)
+
+    return {"video_id": video_id}
+
+
+@app.route("/API/<string:username>/stats/", methods=['PUT', 'PATCH'])
+def AddView(username):
+    video_data = request.get_json()
+    video_id = video_data["id"]
+
+    user_views, video_views = db.AddView(username, video_id)
+
+    return {"user_views": user_views, "video_views": video_views}
 
 
 @app.route("/")
 def Index():
     return render_template("welcome.html",
                            logged_in=fenix_blueprint.session.authorized)
+
+
+@app.route('/logout')
+def LogOut():
+    session.clear()
+    return redirect(url_for("Index"))
 
 
 @app.route("/dashboard")
@@ -85,8 +115,9 @@ def Dashboard():
             # User doesn't exist in DB. Creating new user...
             db.NewUser(username=username, email=email, name=name)
         else:
-            log2term('I', f'User {username} is already registered in the database')
-            
+            log2term('I',
+                     f'User {username} is already registered in the database')
+
             # Checking if user, that already exists, is an admin
             admin = db.GetAdmin(username)
             if (admin != None):
@@ -100,10 +131,31 @@ def Dashboard():
                                is_admin=is_admin)
 
 
-@app.route('/logout')
-def LogOut():
-    session.clear()
-    return redirect(url_for("Index"))
+@app.route('/videos/<int:video_id>')
+def Video(video_id):
+    # Check if user is loggen in
+    if fenix_blueprint.session.authorized == False:
+        return redirect(url_for("fenix-example.login"))
+    else:
+        try:
+            user_data = (
+                fenix_blueprint.session.get("/api/fenix/v1/person/")).json()
+        except TokenExpiredError as e:
+            log2term('E', f'{e.__class__.__name__}. Logging out...')
+            return redirect(url_for("LogOut"))
+
+        # Extract user information
+        username = user_data['username']
+        # Get video information needed for the html template
+        video_info = db.GetVideo(video_id).to_dictionary()
+
+        return render_template("video.html",
+                               video_id=video_id,
+                               video_url=video_info["url"],
+                               video_desc=video_info["desc"],
+                               video_posted_by=video_info["posted_by"],
+                               video_views=video_info["views"],
+                               username=username)
 
 
 ######################## MAIN #########################
